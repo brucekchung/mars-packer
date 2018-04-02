b287aa9c8bbd4389240792379b4173b2ca10645d
Bruce Chung <brucekchung@gmail.com>
2018-04-02 14:23:56 -0600

    Add GET test, POST test WIP

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -11,4 +11,39 @@ const database = require('knex')(configuration)
 
 chai.use(chaiHttp)
 
+describe('API Routes', () => {
+
+  beforeEach((done) => {
+    database.migrate.rollback()
+    .then(() => {
+      database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
+        .then(() => {
+          done()
+        })
+      })
+    })
+  })
+
+  describe('/api/v1/items', () => {
+    it('should have a GET route for items', () => {
+      return chai.request(server)
+      .get('/api/v1/items')
+      .then(response => {
+        response.should.have.status(200)
+        response.should.be.json
+
+        response.body.length.should.equal(3)
+      })
+      .catch(error => {
+        throw error
+      })
+    })
+
+    it('should have a POST route for items', () => {
+
+    })
+  })
+})
 
