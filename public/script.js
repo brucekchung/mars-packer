e3edef7f0643c4e6765b55e425b328b8b8765512
Bruce Chung <brucekchung@gmail.com>
2018-04-02 12:15:10 -0600

    Add delete function to frontend and render WIP

--- public/script.js
+++ public/script.js
@@ -1,7 +1,24 @@
+$(document).ready(() => {
+  console.log('ready')
+  renderItems()
+})
+
+const renderItems = async () => {
+  const allItems = await fetch('/api/v1/items')
+
+  console.log('items: ', await allItems.json())
+}
+
+
 $('.submit-item').on('click', () => {
   const item = $('input').val()
 
-  $('.items').append(`<h3>${item}</h3>`)
+  $('.items').append(`
+    <div class="card">
+      <h3>${item}</h3>
+      <button class="delete">x</button>
+    </div>
+  `)
   //need to add delete button and checked box
 
   fetch('/api/v1/items', {
@@ -14,3 +31,14 @@ $('.submit-item').on('click', () => {
 })
 
 //need to add onload render func
+$('.items').on('click', '.delete', (e) => {
+  const name = $(e.target).closest('.card').find('h3').text() 
+  console.log('name:', name)
+
+  fetch('/api/v1/items', {
+    method: 'DELETE', 
+    body: JSON.stringify({ name }),
+    headers: {'Content-Type': 'application/json'}
+  })
+})
+
